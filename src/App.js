cadb8b7c82ae4747ebe5e5f75f9c0339e2ebcff2
Sarah Deakin <sarah.deakin@starlingbank.com>
2020-08-26 16:16:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+import './styles.css';
+import pelotonlogo from './images/peloton-logo.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    data: null
+  }
+
+  fetchData = () => {
+    axios.get('https://api.onepeloton.com/api/user/sjarquedeakin', {
+      headers: {"accept": "*/*"}}
+    )
+    .then(function(response) {
+      console.log('YOU DID IT GIRL');
+    })
+    .catch(function(error) {
+      console.log('Error on Authentication');
+    });
+  }
+
+  render(){
+  
+ 
+
+ return (
+  <div className="App">
+      <div className="logo">
+        <img src={pelotonlogo} height= "50%" width="50%" alt="logo" />
+      </div>
+    <h1>Sarah's Peloton API</h1>
+
+    <div>
+        <button className="fetch-button" onClick={this.fetchData}>
+          Fetch Last Type Of Workout
+        </button>
+        <br />
+      </div>
+  </div>
+ );
+}}
 
 export default App;
